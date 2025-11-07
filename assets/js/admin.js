93cc5294e01a9a97749cfb8cf7e82648374526e1
Emanuelecozzolino1926 <emanuele.cozzolino18@gmail.com>
2025-11-07 10:41:50 +0100

    first commit

---
+++ assets/js/admin.js
@@ -0,0 +1,5 @@
+const apiUrl = "https://striveschool-api.herokuapp.com/api/product/";
+const keyApi =
+  "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJfaWQiOiI2OTBkYmI0YmY0YmQ0NzAwMTU4NWIxZWIiLCJpYXQiOjE3NjI1MDc1OTUsImV4cCI6MTc2MzcxNzE5NX0.M8nwzXbxPyuNyxY2Oj0qTF_vs-LNm9uJSZ4g59LrWm";
+
+fetch(apiUrl, {});
